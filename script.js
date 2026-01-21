d131d58c215352a266a1b4a91dbb6edfcd9ad535
celsve-cmd <celsve@gmail.com>
2026-01-21 14:28:00 +0100

    ---

---
+++ script.js
@@ -0,0 +1,14 @@
+function changeTextAndColor() {
+    document.getElementById('myDiv').innerHTML = 'Hahahaha! Kanskje ikke?'
+    document.getElementById('myDiv').style.backgroundColor = 'lightyellow'
+}
+
+function changeButtonBack(){
+    document.getElementById('myDiv').innerHTML = 'Kanskje denne?'
+    document.getElementById('myDiv').classList.style.backgroundColor = 'none'
+}
+
+
+
+
+
